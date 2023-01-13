b5f2420e2ad4fd0a9046eda2abfac6289299f1a9
Jairo Evaristo <jairoevaristodev@gmail.com>
2023-01-13 20:06:49 -0300

    Delete frontend directory

--- frontend/src/utils/filterStatusOrder.ts
+++
@@ -1,8 +0,0 @@
-import { Order } from "../types/Order";
-
-export function filterStatusOrder(
-  status: "WAITING" | "IN_PRODUCTION" | "DONE",
-  orders: Order[]
-) {
-  return orders.filter((order) => order.status === status);
-}
--- frontend/src/utils/formatCurrency.ts
+++
@@ -1,6 +0,0 @@
-export const formatCurrency = (value: number) => {
-  return new Intl.NumberFormat('pt-br', {
-    style: 'currency', 
-    currency: 'BRL'
-  }).format(value);
-}
